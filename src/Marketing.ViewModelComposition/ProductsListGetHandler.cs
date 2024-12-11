193ea322733ff56b3be926f0c4c0df7a03a1ca83
Andreas Öhlund <andreas.ohlund@particular.net>
2024-12-11 09:16:35 +0100

    Cleanup

--- src/Marketing.ViewModelComposition/ProductsListGetHandler.cs
+++ src/Marketing.ViewModelComposition/ProductsListGetHandler.cs
@@ -7,22 +7,15 @@ using ServiceComposer.AspNetCore;
 using ITOps.ViewModelComposition;
 using System.Dynamic;
 
-class ProductsListGetHandler : ICompositionRequestsHandler
+class ProductsListGetHandler(HttpClient httpClient) : ICompositionRequestsHandler
 {
-    readonly HttpClient httpClient;
-
-    public ProductsListGetHandler(HttpClient httpClient)
-    {
-        this.httpClient = httpClient;
-    }
-
     [HttpGet("/products")]
     public async Task Handle(HttpRequest request)
     {
         var url = "http://localhost:50688/product";
         var response = await httpClient.GetAsync(url);
 
-        var availableProducts = await response.Content.AsExpandoArray();
+        dynamic[] availableProducts = await response.Content.AsExpandoArray();
         var availableProductsViewModel = MapToViewModelDictionary(availableProducts);
 
         var context = request.GetCompositionContext();
@@ -42,6 +35,7 @@ class ProductsListGetHandler : ICompositionRequestsHandler
         foreach (var product in products)
         {
             dynamic productDetailObject = new ExpandoObject();
+            
             productDetailObject.ProductId = product.ProductId;
             productDetailObject.Name = product.Name;
             productDetailObject.ImageUrl = product.ImageUrl;
